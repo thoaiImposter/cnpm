547d7a9834a988ef66829256ebde61446cdbb94c
TNgan24 <146104057+TNgan24@users.noreply.github.com>
2025-05-20 21:35:50 +0700

    update ManageDrinkAdapter

--- app/src/main/java/com/app/buchin/adapter/ManageDrinkAdapter.java
+++ app/src/main/java/com/app/buchin/adapter/ManageDrinkAdapter.java
@@ -15,7 +15,7 @@ import com.app.buchin.model.Drink;
 
 import java.util.List;
 
-public class ManageDrinkAdapter extends RecyclerView.Adapter<ManageDrinkAdapter.ManageDrinkViewHolder> {
+public class ManageDrinkAdapter  {
 
     private final List<Drink> mListDrink;
     private final IManagerDrinkListener iManagerDrinkListener;
@@ -29,51 +29,5 @@ public class ManageDrinkAdapter extends RecyclerView.Adapter<ManageDrinkAdapter.
         this.iManagerDrinkListener = iManagerDrinkListener;
     }
 
-    @NonNull
-    @Override
-    public ManageDrinkAdapter.ManageDrinkViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_manage_drink, parent, false);
-        return new ManageDrinkAdapter.ManageDrinkViewHolder(view);
-    }
 
-    @Override
-    public void onBindViewHolder(@NonNull ManageDrinkAdapter.ManageDrinkViewHolder holder, int position) {
-        Drink drink = mListDrink.get(position);
-        if (drink == null) {
-            return;
-        }
-        holder.tvName.setText(drink.getName());
-        String strCurrentQuantity = drink.getQuantity() + " " + drink.getUnitName();
-        holder.tvCurrentQuantity.setText(strCurrentQuantity);
-
-        // Listener
-        holder.layoutItem.setOnClickListener(new IOnSingleClickListener() {
-            @Override
-            public void onSingleClick(View v) {
-                iManagerDrinkListener.clickItem(drink);
-            }
-        });
-    }
-
-    @Override
-    public int getItemCount() {
-        if (mListDrink != null) {
-            return mListDrink.size();
-        }
-        return 0;
-    }
-
-    public static class ManageDrinkViewHolder extends RecyclerView.ViewHolder {
-
-        private final TextView tvName;
-        private final TextView tvCurrentQuantity;
-        private final RelativeLayout layoutItem;
-
-        public ManageDrinkViewHolder(@NonNull View itemView) {
-            super(itemView);
-            tvName = itemView.findViewById(R.id.tv_name);
-            tvCurrentQuantity = itemView.findViewById(R.id.tv_current_quantity);
-            layoutItem = itemView.findViewById(R.id.layout_item);
-        }
-    }
 }
